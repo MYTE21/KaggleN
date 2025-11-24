3f966c18996e388e5a68caa13db2523ad1b28d35
MYTE21 <yasmi.tohabar@gmail.com>
2025-11-24 17:45:37 +0600

    The setup.py file and the initial Kaggle note info agent were created.

---
+++ utilities/setup.py
@@ -0,0 +1,69 @@
+import os
+from dotenv import load_dotenv
+from IPython.display import Markdown
+
+# Google ADK & GenAI Imports.
+from google.adk.agents import Agent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import google_search
+
+from google.genai import types
+
+
+def get_google_api_key(use_vertexai: bool = True) -> str | None:
+    """
+    Configures the environment for Google Gen AI authentication.
+
+    This function loads environment variables, ensures the Google API key is present,
+    and configures the backend mode (Vertex AI vs. Gemini Developer API/AI Studio)
+    by manipulating the 'GOOGLE_GENAI_USE_VERTEXAI'
+    environment variable.
+
+    Parameters:
+        use_vertexai (bool): Determines the backend configuration.
+            - If False: Explicitly sets 'GOOGLE_GENAI_USE_VERTEXAI' to "FALSE" to force the usage of the Gemini
+            Developer API (AI Studio).
+            - If True (Default): Removes 'GOOGLE_GENAI_USE_VERTEXAI' from the environment,
+            if it exists, allowing the SDK to revert to its default behavior or pick up Vertex AI credentials.
+
+    Returns:
+        str | None: The loaded Google API Key if successful, or None if the key is missing from the environment.
+    """
+    load_dotenv()
+
+    google_api_key = os.getenv("GOOGLE_API_KEY")
+
+    if not google_api_key:
+        print("⚠️ Authentication Error: 'GOOGLE_API_KEY' environment variable is missing.")
+        return None
+
+    try:
+        os.environ["GOOGLE_API_KEY"] = google_api_key
+        if not use_vertexai:
+            os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "FALSE"
+        else:
+            os.environ.pop("GOOGLE_GENAI_USE_VERTEXAI", None)
+
+        print("✅ Gemini API key setup complete.")
+        return google_api_key
+    except Exception as e:
+        print(f"⚠️ Error setting environment variables: {e}")
+
+
+def get_retry_config() -> types.HttpRetryOptions:
+    """
+    Creates a retry configuration for the Google Gen AI Client.
+    """
+    retry_config = types.HttpRetryOptions(
+        attempts=5,
+        exp_base=7,
+        initial_delay=1,
+        http_status_codes=[429, 500, 503, 504]
+    )
+
+    return retry_config
+
+
+if __name__ == "__main__":
+    pass
