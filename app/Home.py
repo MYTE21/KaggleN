90cd4ecd6dd0e335a89032be73898aec4a6afe67
MYTE21 <yasmi.tohabar@gmail.com>
2025-11-27 17:44:21 +0600

    Initial connection with the Google ADK.

--- app/Home.py
+++ app/Home.py
@@ -1,6 +1,95 @@
 import os
 import streamlit as st
 from components import title, chat
+import asyncio
+from google.genai import types
+from dotenv import load_dotenv
+from google.adk.models.google_llm import Gemini
+from google.adk.agents import Agent, LlmAgent
+from google.adk.tools import google_search
+from google.adk.runners import Runner, InMemoryRunner
+
+
+load_dotenv()
+
+
+def get_google_api_key(use_vertexai: bool = True) -> str | None:
+    """
+    Configures the environment for Google Gen AI authentication.
+
+    This function loads environment variables, ensures the Google API key is present,
+    and configures the backend mode (Vertex AI vs. Gemini Developer API/AI Studio)
+    by manipulating the 'GOOGLE_GENAI_USE_VERTEXAI'
+    environment variable.
+
+    Parameters:
+        use_vertexai (bool): Determines the backend configuration.
+            - If False: Explicitly sets 'GOOGLE_GENAI_USE_VERTEXAI' to "FALSE" to force the usage of the Gemini
+            Developer API (AI Studio).
+            - If True (Default): Removes 'GOOGLE_GENAI_USE_VERTEXAI' from the environment,
+            if it exists, allowing the SDK to revert to its default behavior or pick up Vertex AI credentials.
+
+    Returns:
+        str | None: The loaded Google API Key if successful, or None if the key is missing from the environment.
+    """
+    google_api_key = os.getenv("GOOGLE_API_KEY")
+
+    if not google_api_key:
+        print("⚠️ Authentication Error: 'GOOGLE_API_KEY' environment variable is missing.")
+        return None
+
+    try:
+        os.environ["GOOGLE_API_KEY"] = google_api_key
+        if not use_vertexai:
+            os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "FALSE"
+        else:
+            os.environ.pop("GOOGLE_GENAI_USE_VERTEXAI", None)
+
+        print("✅ Gemini API key setup complete.")
+        return google_api_key
+    except Exception as e:
+        print(f"⚠️ Error setting environment variables: {e}")
+
+
+def get_retry_config() -> types.HttpRetryOptions:
+    """
+    Creates a retry configuration for the Google Gen AI Client.
+    """
+    retry_config = types.HttpRetryOptions(
+        attempts=5,
+        exp_base=7,
+        initial_delay=1,
+        http_status_codes=[429, 500, 503, 504]
+    )
+
+    return retry_config
+
+
+async def gemini_agent(message: str):
+    if "root_agent" not in st.session_state:
+        st.session_state.root_agent = Agent(
+            name="helpful_assistant",
+            model=Gemini(
+                model="gemini-2.5-flash-lite",
+                retry_options=get_retry_config()
+            ),
+            description="A simple agent that can answer general questions.",
+            instruction="""
+                You are a helpful and confident assistant. 
+                You are capable of searching Google for recent information using google_search tool. 
+                Use google_search for current information or if unsure. 
+                Whenever you reference information from Google Search, include the link in your answer. 
+                Create markdown formating, e.g., table, bulleted or numerical points, headings, 
+                and quotes if necessary. Also, consider creating a mermaid diagram if needed.
+                """,
+            tools=[google_search]
+        )
+
+    runner = InMemoryRunner(agent=st.session_state.root_agent)
+
+    response = await runner.run_debug(message)
+    return response[0].content.parts[0].text
+
 
 # -- Setup --
 LOGO_PATH = os.path.join(os.getcwd(), "assets/icon.png")
@@ -37,6 +126,7 @@ if prompt := st.chat_input("Ask me anything..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     with st.chat_message("assistant"):
-        response = st.write_stream(chat.random_response())
+        # response = st.write_stream(chat.random_response())
+        response = st.write(asyncio.run(gemini_agent(prompt)))
 
     st.session_state.messages.append({"role": "assistant", "content": response})
--- app/components/chat.py
+++ app/components/chat.py
@@ -1,8 +1,13 @@
 import random
 import time
-from typing import Any, Generator
+from typing import Generator, Any
+import streamlit as st
+
+# GOOGLE_API_KEY = setup.get_google_api_key(False)
+# retry_config = setup.get_retry_config()
 
 
+# -- Random Response --
 def random_response() -> Generator[str, None, None]:
     response = random.choice([
         "The Amazon rainforest produces around 20% of the world’s oxygen; do you know that kind of massive ecosystem is still shrinking every year?",
@@ -13,3 +18,34 @@ def random_response() -> Generator[str, None, None]:
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
+
+#
+# # AI Agent
+# async def gemini_agent(message: str) -> Any:
+#     if "root_agent" not in st.session_state:
+#         st.session_state.root_agent = Agent(
+#             name="helpful_assistant",
+#             model=Gemini(
+#                 model="gemini-2.5-flash-lite",
+#                 retry_options=retry_config
+#             ),
+#             description="A simple agent that can answer general questions.",
+#             instruction="""
+#                 You are a helpful and confident assistant.
+#                 You are capable of searching Google for recent information using google_search tool.
+#                 Use google_search for current information or if unsure.
+#                 Whenever you reference information from Google Search, include the link in your answer.
+#                 Create markdown formating, e.g., table, bulleted or numerical points, headings,
+#                 and quotes if necessary. Also, consider creating a mermaid diagram if needed.
+#                 """,
+#             tools=[google_search]
+#         )
+#
+#     runner = InMemoryRunner(agent=st.session_state.root_agent)
+#
+#     response = await runner.run_debug(message)
+#     return response
+
+
+if __name__ == "__main__":
+    pass
